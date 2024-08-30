f9ca2f2c528621ede5bd56779cb719f673f414a4
Christopher Ariza <ariza@flexatone.com>
2024-08-30 14:51:52 -0700

    improvements to VersionSpec, DepSpec

--- src/dep_spec.rs
+++ src/dep_spec.rs
@@ -6,15 +6,15 @@ use pest::Parser;
 use pest_derive::Parser;
 
 use crate::version_spec::VersionSpec;
+// use crate::version_spec::VersionPart;
 
-
+// This is a redimentary grammar for https://packaging.python.org/en/latest/specifications/dependency-specifiers/
 #[derive(Parser)]
 #[grammar = "dep_spec.pest"]
 struct DepSpecParser;
 
-
-#[derive(Debug)]
-enum VersionOperator {
+#[derive(Debug, PartialEq)]
+enum DepOperator {
     LessThan,
     LessThanOrEqual,
     Equal,
@@ -25,35 +25,34 @@ enum VersionOperator {
     ExactMatch,
 }
 
-impl FromStr for VersionOperator {
+impl FromStr for DepOperator {
     type Err = Box<dyn Error>;
-
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "<" => Ok(VersionOperator::LessThan),
-            "<=" => Ok(VersionOperator::LessThanOrEqual),
-            "==" => Ok(VersionOperator::Equal),
-            "!=" => Ok(VersionOperator::NotEqual),
-            ">" => Ok(VersionOperator::GreaterThan),
-            ">=" => Ok(VersionOperator::GreaterThanOrEqual),
-            "~=" => Ok(VersionOperator::Compatible),
-            "===" => Ok(VersionOperator::ExactMatch),
+            "<" => Ok(DepOperator::LessThan),
+            "<=" => Ok(DepOperator::LessThanOrEqual),
+            "==" => Ok(DepOperator::Equal),
+            "!=" => Ok(DepOperator::NotEqual),
+            ">" => Ok(DepOperator::GreaterThan),
+            ">=" => Ok(DepOperator::GreaterThanOrEqual),
+            "~=" => Ok(DepOperator::Compatible),
+            "===" => Ok(DepOperator::ExactMatch),
             _ => Err(format!("Unknown operator: {}", s).into()),
         }
     }
 }
 
-impl fmt::Display for VersionOperator {
+impl fmt::Display for DepOperator {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let op_str = match self {
-            VersionOperator::LessThan => "<",
-            VersionOperator::LessThanOrEqual => "<=",
-            VersionOperator::Equal => "==",
-            VersionOperator::NotEqual => "!=",
-            VersionOperator::GreaterThan => ">",
-            VersionOperator::GreaterThanOrEqual => ">=",
-            VersionOperator::Compatible => "~=",
-            VersionOperator::ExactMatch => "===",
+            DepOperator::LessThan => "<",
+            DepOperator::LessThanOrEqual => "<=",
+            DepOperator::Equal => "==",
+            DepOperator::NotEqual => "!=",
+            DepOperator::GreaterThan => ">",
+            DepOperator::GreaterThanOrEqual => ">=",
+            DepOperator::Compatible => "~=",
+            DepOperator::ExactMatch => "===",
         };
         write!(f, "{}", op_str)
     }
@@ -63,7 +62,7 @@ impl fmt::Display for VersionOperator {
 #[derive(Debug)]
 struct DepSpec {
     name: String,
-    operators: Vec<VersionOperator>,
+    operators: Vec<DepOperator>,
     versions: Vec<VersionSpec>,
 }
 
@@ -72,51 +71,72 @@ impl DepSpec {
         let parse_result = DepSpecParser::parse(Rule::name_req, input)
             .map_err(|e| format!("Parsing error: {}", e))?;
 
-        let reuslts = parse_result.into_iter().next().unwrap();
+        let results = parse_result.into_iter().next().unwrap();
         let mut package_name = String::new();
         let mut versions = Vec::new();
         let mut operators = Vec::new();
 
-        for pairs in reuslts.into_inner() {
+        for pairs in results.into_inner() {
             match pairs.as_rule() {
                 Rule::identifier => {
                     package_name = pairs.as_str().to_string();
                 }
                 Rule::version_many => {
-                    for version_pair in pairs.into_inner() {
-                        if version_pair.as_rule() == Rule::version_one {
+                    for capture in pairs.into_inner() {
+                        // print!("{:?}", capture);
+                        if capture.as_rule() == Rule::version_one {
                             let mut op = None;
                             let mut version = String::new();
 
-                            for v_pair in version_pair.into_inner() {
+                            for v_pair in capture.into_inner() {
                                 match v_pair.as_rule() {
                                     Rule::version_cmp => {
-                                        op = Some(v_pair.as_str().parse::<VersionOperator>()
+                                        op = Some(v_pair.as_str().parse::<DepOperator>()
                                             .map_err(|e| e.to_string())?);
                                     }
                                     Rule::version => version = v_pair.as_str().to_string(),
                                     _ => {}
                                 }
                             }
-
                             if let Some(op) = op {
                                 operators.push(op);
-                                versions.push(VersionSpec::new(&version));                            }
+                                versions.push(VersionSpec::new(&version));
+                            }
                         }
                     }
                 }
                 _ => {}
             }
         }
-
+        if operators.len() != versions.len() {
+            return Err("Unmatched versions and operators".to_string());
+        }
         Ok(DepSpec {
             name: package_name,
             operators,
             versions,
         })
     }
+    pub fn validate(&self, version: &VersionSpec) -> bool {
+        for (op, spec_version) in self.operators.iter().zip(&self.versions) {
+            // println!("{:?} spec_version {:?} version {:?}", op, spec_version, version);
+            let is_compatible = match op {
+                DepOperator::LessThan => version < spec_version,
+                DepOperator::LessThanOrEqual => version <= spec_version,
+                DepOperator::Equal => version == spec_version,
+                DepOperator::NotEqual => version != spec_version,
+                DepOperator::GreaterThan => version > spec_version,
+                DepOperator::GreaterThanOrEqual => version >= spec_version,
+                DepOperator::Compatible => version.is_major_compatible(spec_version),
+                DepOperator::ExactMatch => version == spec_version,
+            };
+            if !is_compatible {
+                return false;
+            }
+        }
+        true
+    }
 }
-
 
 //------------------------------------------------------------------------------
 #[cfg(test)]
@@ -126,9 +146,69 @@ mod tests {
 
     #[test]
     fn test_dep_spec_a() {
-
         let input = "package>=0.2,<0.3";
         let ds1 = DepSpec::new(input).unwrap();
+        assert_eq!(ds1.name, "package");
+        assert_eq!(ds1.operators[0], DepOperator::GreaterThanOrEqual);
+        assert_eq!(ds1.operators[1], DepOperator::LessThan);
+    }
+
+    #[test]
+    fn test_dep_spec_b() {
+        let input = "package[foo]>=0.2; python_version < '2.7'";
+        let ds1 = DepSpec::new(input).unwrap();
+        // println!("{:?}", ds1);
+        assert_eq!(ds1.name, "package");
+        assert_eq!(ds1.operators[0], DepOperator::GreaterThanOrEqual);
+        assert_eq!(ds1.versions[0], VersionSpec::new("0.2"));
+
+    }
+
+    #[test]
+    fn test_dep_spec_c() {
+        let input = "package==0.2<=";
+        let ds1 = DepSpec::new(input).unwrap();
+        assert_eq!(ds1.name, "package");
+    }
+
+    #[test]
+    fn test_dep_spec_validate_a() {
+        let input = "package>0.2<2.0";
+        let ds1 = DepSpec::new(input).unwrap();
+        assert_eq!(ds1.name, "package");
+        assert_eq!(ds1.validate(&VersionSpec::new("0.3")), true);
+        assert_eq!(ds1.validate(&VersionSpec::new("0.2")), false);
+        assert_eq!(ds1.validate(&VersionSpec::new("0.2.1")), true);
+    }
+
+    #[test]
+    fn test_dep_spec_validate_b() {
+        let input = "package>0.2,<2.0";
+        let ds1 = DepSpec::new(input).unwrap();
+        assert_eq!(ds1.validate(&VersionSpec::new("2.0.1")), false);
+        assert_eq!(ds1.validate(&VersionSpec::new("2.0.0")), false);
+        assert_eq!(ds1.validate(&VersionSpec::new("1.9.99.99999")), true);
+    }
+
+    #[test]
+    fn test_dep_spec_validate_c() {
+        let input = "package>=2.0,<=3.0";
+        let ds1 = DepSpec::new(input).unwrap();
+        assert_eq!(ds1.validate(&VersionSpec::new("2.0")), true);
+        assert_eq!(ds1.validate(&VersionSpec::new("1.9.99.99999")), false);
+        assert_eq!(ds1.validate(&VersionSpec::new("3.0")), true);
+
+    }
+
+    #[test]
+    fn test_dep_spec_validate_d() {
+        let input = "package==2.*";
+        let ds1 = DepSpec::new(input).unwrap();
         println!("{:?}", ds1);
+
+        assert_eq!(ds1.validate(&VersionSpec::new("2.4")), true);
+        assert_eq!(ds1.validate(&VersionSpec::new("1.9.99.99999")), false);
+        assert_eq!(ds1.validate(&VersionSpec::new("3.0")), false);
+        assert_eq!(ds1.validate(&VersionSpec::new("2.3")), true);
     }
 }
--- src/version_spec.rs
+++ src/version_spec.rs
@@ -10,7 +10,7 @@ enum VersionPart {
 }
 
 //------------------------------------------------------------------------------
-#[derive(Debug, PartialEq, Eq, Clone, Hash)]
+#[derive(Debug, Clone, Hash)]
 pub(crate) struct VersionSpec(Vec<VersionPart>);
 
 impl VersionSpec {
@@ -27,10 +27,73 @@ impl VersionSpec {
             .collect();
         VersionSpec(parts)
     }
+    pub fn is_major_compatible(&self, other: &Self) -> bool {
+        if let (Some(VersionPart::Number(self_major)), Some(VersionPart::Number(other_major))) =
+            (self.0.get(0), other.0.get(0)) {
+            return self_major == other_major;
+        }
+        false
+    }
+    // pub fn is_compatible(&self, other: &Self) -> bool {
+    //     for (self_part, other_part) in self.0.iter().zip(&other.0) {
+    //         match (self_part, other_part) {
+    //             (VersionPart::Number(a), VersionPart::Number(b)) => {
+    //                 if a != b {
+    //                     return false;
+    //                 }
+    //             }
+    //             (VersionPart::Text(a), VersionPart::Text(b)) => {
+    //                 if a != "*" && b != "*" && a != b {
+    //                     return false;
+    //                 }
+    //             }
+    //             // If one part is a number and the other is text, handle "*" as a wildcard
+    //             (VersionPart::Number(_), VersionPart::Text(b)) if b == "*" => continue,
+    //             (VersionPart::Text(a), VersionPart::Number(_)) if a == "*" => continue,
+    //             // Incompatible if none of the above conditions hold
+    //             _ => return false,
+    //         }
+    //     }
+    //     true
+    // }
+
 }
 impl Ord for VersionSpec {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.0.cmp(&other.0)
+        println!("cmp: {:?} {:?}", self, other);
+
+        for (self_part, other_part) in self.0.iter().zip(&other.0) {
+            println!("here: {:?} {:?}", self_part, other_part);
+
+            let ordering = match (self_part, other_part) {
+                (VersionPart::Number(a), VersionPart::Number(b)) => a.cmp(b),
+                (VersionPart::Text(a), VersionPart::Text(b)) => {
+                    if a == "*" || b == "*" {
+                        Ordering::Equal
+                    } else {
+                        a.cmp(b)
+                    }
+                }
+                (VersionPart::Number(_), VersionPart::Text(b)) => {
+                    if b == "*" {
+                        Ordering::Equal
+                    } else {
+                        Ordering::Greater // numbers are always greater than text
+                    }
+                }
+                (VersionPart::Text(a), VersionPart::Number(_)) => {
+                    if a == "*" {
+                        Ordering::Equal
+                    } else {
+                        Ordering::Less
+                    }
+                }
+            };
+            if ordering != Ordering::Equal {
+                return ordering;
+            }
+        }
+        self.0.len().cmp(&other.0.len()) // version with more components is greater
     }
 }
 impl PartialOrd for VersionSpec {
@@ -38,6 +101,54 @@ impl PartialOrd for VersionSpec {
         Some(self.cmp(other))
     }
 }
+impl PartialEq for VersionSpec {
+    fn eq(&self, other: &Self) -> bool {
+        for (self_part, other_part) in self.0.iter().zip(&other.0) {
+            match (self_part, other_part) {
+                // If either part is a wildcard "*", consider them equal
+                (VersionPart::Text(a), VersionPart::Text(b)) if a == "*" || b == "*" => continue,
+                (VersionPart::Text(a), VersionPart::Number(_)) if a == "*" => continue,
+                (VersionPart::Number(_), VersionPart::Text(b)) if b == "*" => continue,
+                // Otherwise, parts must match exactly
+                (VersionPart::Number(a), VersionPart::Number(b)) if a != b => return false,
+                (VersionPart::Text(a), VersionPart::Text(b)) if a != b => return false,
+                // If types differ and no wildcard is involved, they are not equal
+                (VersionPart::Number(_), VersionPart::Text(_)) => return false,
+                (VersionPart::Text(_), VersionPart::Number(_)) => return false,
+                _ => {} // continue
+            }
+        }
+        true
+    }
+}
+
+impl Eq for VersionSpec {}
 
 
 
+
+//------------------------------------------------------------------------------
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_version_spec_a() {
+        assert_eq!(VersionSpec::new("2.2"), VersionSpec::new("2.2"));
+        assert_eq!(VersionSpec::new("2.*"), VersionSpec::new("2.2"));
+        assert_eq!(VersionSpec::new("2.2"), VersionSpec::new("2.*"));
+    }
+    #[test]
+    fn test_version_spec_b() {
+        assert_eq!(VersionSpec::new("2.*.1"), VersionSpec::new("2.2.1"));
+        assert_ne!(VersionSpec::new("2.*.1"), VersionSpec::new("2.2.2"));
+    }
+    #[test]
+    fn test_version_spec_c() {
+        // NOTE: not sure these falses are what we want
+        assert_eq!(VersionSpec::new("2.*") > VersionSpec::new("2.2.1"), false);
+        assert_eq!(VersionSpec::new("2.2") > VersionSpec::new("2.*"), false);
+    }
+
+}
