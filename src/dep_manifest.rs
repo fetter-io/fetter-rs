7d2fc35248be7d187f0d3280cd05575451b54581
Christopher Ariza <ariza@flexatone.com>
2024-09-02 16:48:08 -0700

    additional testing of DepManifest::from_requirements

--- src/dep_manifest.rs
+++ src/dep_manifest.rs
@@ -1,12 +1,11 @@
 use std::collections::HashMap;
-// use std::fs;
-// use std::path::Path;
-// use std::process::Command;
-// use tempfile::tempdir;
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::path::PathBuf;
 
 use crate::dep_spec::DepSpec;
 use crate::package::Package;
-
 
 #[derive(Debug)]
 struct DepManifest {
@@ -29,22 +28,18 @@ impl DepManifest {
         }
         Ok(DepManifest { dep_specs })
     }
-
-    // pub fn from_requirements_file<P: AsRef<Path>>(file_path: P) -> Result<Self, String> {
-    //     let contents = fs::read_to_string(file_path)
-    //         .map_err(|e| format!("Failed to read requirements file: {}", e))?;
-    //     let lines = contents.lines();
-    //     let mut packages = HashMap::new();
-
-    //     for line in lines {
-    //         let trimmed_line = line.trim();
-    //         if !trimmed_line.is_empty() && !trimmed_line.starts_with("#") {
-    //             let dep_spec = DepSpec::new(trimmed_line)?;
-    //             packages.insert(dep_spec.name.clone(), dep_spec);
-    //         }
-    //     }
-    //     Ok(DepManifest { packages })
-    // }
+    pub fn from_requirements(file_path: &PathBuf) -> Result<Self, String> {
+        let file = File::open(file_path).map_err(|e| format!("Failed to open file: {}", e))?;
+        let lines = io::BufReader::new(file).lines();
+        let filtered_lines = lines.filter_map(|line| {
+            match line {
+                Ok(s) if !s.is_empty() && !s.starts_with('#') => Some(s),  // Return String directly
+                Ok(_) => None,  // Skip empty or commented lines
+                Err(_) => None, // Ignore lines that failed to read
+            }
+        });
+        DepManifest::from_iter(filtered_lines)
+    }
 
     // pub fn from_pyproject_toml<P: AsRef<Path>>(file_path: P) -> Result<Self, String> {
     //     let contents = fs::read_to_string(file_path)
@@ -89,7 +84,9 @@ impl DepManifest {
     //     // TempDir will be cleaned up when it goes out of scope
     //     Ok(manifest)
     // }
-
+    pub fn len(&self) -> usize {
+        self.dep_specs.len()
+    }
     pub fn validate(&self, package: &Package) -> bool {
         if let Some(dep_spec) = self.dep_specs.get(&package.name) {
             dep_spec.validate_version(&package.version)
@@ -104,6 +101,8 @@ impl DepManifest {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use tempfile::tempdir;
+    use std::io::Write;
 
     #[test]
     fn test_dep_spec_a() {
@@ -123,5 +122,34 @@ mod tests {
 
         let p3 = Package::from_dist_info("pk1-0.3.0.dist-info").unwrap();
         assert_eq!(dm.validate(&p3), false);
+    }
+
+    #[test]
+    fn test_from_requirements_a() {
+        // Create a temporary directory and file
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("requirements.txt");
+
+        // Write test content to the temp file
+        let mut file = File::create(&file_path).unwrap();
+        writeln!(file, "# comment").unwrap();
+        writeln!(file, "pk1>=0.2,  <0.3    ").unwrap();
+        writeln!(file, "").unwrap();
+        writeln!(file, "pk2>=1,<3").unwrap();
+        writeln!(file, "# ").unwrap();
+
+        let dep_manifest = DepManifest::from_requirements(&file_path).unwrap();
+        assert_eq!(dep_manifest.len(), 2);
+
+        let p1 = Package::from_name_and_version("pk2", "2.1").unwrap();
+        assert_eq!(dep_manifest.validate(&p1), true);
+        let p2 = Package::from_name_and_version("pk2", "0.1").unwrap();
+        assert_eq!(dep_manifest.validate(&p2), false);
+        let p3 = Package::from_name_and_version("pk1", "0.2.2.999").unwrap();
+        assert_eq!(dep_manifest.validate(&p3), true);
+
+        let p4 = Package::from_name_and_version("pk99", "0.2.2.999").unwrap();
+        assert_eq!(dep_manifest.validate(&p4), false);
+
     }
 }
