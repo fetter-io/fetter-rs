361f0dd1d9a4c8bd3ab8317f5d7318b56b02872d
Christopher Ariza <ariza@flexatone.com>
2024-09-13 11:41:27 -0700

    implemetned library interface

--- src/main.rs
+++ src/main.rs
@@ -1,200 +1,207 @@
-mod dep_manifest;
-mod dep_spec;
-mod exe_search;
-mod package;
-mod scan_fs;
-mod validation;
-mod version_spec;
+// mod dep_manifest;
+// mod dep_spec;
+// mod exe_search;
+// mod package;
+// mod scan_fs;
+// mod validation;
+// mod version_spec;
 
-use crate::dep_manifest::DepManifest;
-use crate::scan_fs::Anchor;
-use crate::scan_fs::ScanFS;
+use std;
 
-use clap::{Parser, Subcommand, ValueEnum};
-use std::path::PathBuf;
+// use crate::dep_manifest::DepManifest;
+// use crate::scan_fs::Anchor;
+// use crate::scan_fs::ScanFS;
 
-// NEXT:
-// Packages collect url for @ validations
-// takes a requirements bound file and validates
-// Implement a colorful display
-// Implement a monitoring mode
+// use clap::{Parser, Subcommand, ValueEnum};
+// use std::path::PathBuf;
 
-//------------------------------------------------------------------------------
-// utility enums
-
-#[derive(Copy, Clone, ValueEnum)]
-pub(crate) enum CliAnchor {
-    Lower,
-    Upper,
-    Both,
-}
-impl From<CliAnchor> for Anchor {
-    fn from(cli_anchor: CliAnchor) -> Self {
-        match cli_anchor {
-            CliAnchor::Lower => Anchor::Lower,
-            CliAnchor::Upper => Anchor::Upper,
-            CliAnchor::Both => Anchor::Both,
-        }
-    }
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    fetter::run_cli(std::env::args_os());
+    Ok(())
 }
 
-//------------------------------------------------------------------------------
+// // NEXT:
+// // Packages collect url for @ validations
+// // takes a requirements bound file and validates
+// // Implement a colorful display
+// // Implement a monitoring mode
 
-const AFTER_HELP: &str = "\
-Examples:
-  fetter validate --bound requirements.txt display
-  fetter --exe python3 validate --bound requirements.txt display
+// //------------------------------------------------------------------------------
+// // utility enums
 
-  fetter scan display
-  fetter derive write -o /tmp/bond_requirements.txt
-  fetter purge
-";
+// #[derive(Copy, Clone, ValueEnum)]
+// pub(crate) enum CliAnchor {
+//     Lower,
+//     Upper,
+//     Both,
+// }
+// impl From<CliAnchor> for Anchor {
+//     fn from(cli_anchor: CliAnchor) -> Self {
+//         match cli_anchor {
+//             CliAnchor::Lower => Anchor::Lower,
+//             CliAnchor::Upper => Anchor::Upper,
+//             CliAnchor::Both => Anchor::Both,
+//         }
+//     }
+// }
 
-#[derive(clap::Parser)]
-#[command(version, about, long_about = None, after_help = AFTER_HELP)]
-struct Cli {
-    /// Zero or more executable paths to derive site package locations. If not provided, all discoverable executables will be used.
-    #[arg(short, long, value_name = "FILES", required = false)]
-    exe: Option<Vec<PathBuf>>,
+// //------------------------------------------------------------------------------
 
-    #[command(subcommand)]
-    command: Option<Commands>,
-}
+// const AFTER_HELP: &str = "\
+// Examples:
+//   fetter validate --bound requirements.txt display
+//   fetter --exe python3 validate --bound requirements.txt display
 
-#[derive(Subcommand)]
-enum Commands {
-    /// Validate packages in an environment
-    Validate {
-        /// File path from which to read bound requirements.
-        #[arg(short, long, value_name = "FILE")]
-        bound: Option<PathBuf>,
+//   fetter scan display
+//   fetter derive write -o /tmp/bond_requirements.txt
+//   fetter purge
+// ";
 
-        #[command(subcommand)]
-        subcommands: ValidateSubcommand,
-    },
-    /// Scan environment to report on installed packages.
-    Scan {
-        #[command(subcommand)]
-        subcommands: ScanSubcommand,
-    },
-    /// Scan environment to report on installed packages.
-    Derive {
-        // Select the nature of the bound in the derived requirements.
-        #[arg(short, long, value_enum)]
-        anchor: CliAnchor,
+// #[derive(clap::Parser)]
+// #[command(version, about, long_about = None, after_help = AFTER_HELP)]
+// struct Cli {
+//     /// Zero or more executable paths to derive site package locations. If not provided, all discoverable executables will be used.
+//     #[arg(short, long, value_name = "FILES", required = false)]
+//     exe: Option<Vec<PathBuf>>,
 
-        #[command(subcommand)]
-        subcommands: DeriveSubcommand,
-    },
-    /// Purge packages that fail validation
-    Purge {
-        /// File path from which to read bound requirements.
-        #[arg(short, long, value_name = "FILE")]
-        bound: Option<PathBuf>,
-    },
-}
+//     #[command(subcommand)]
+//     command: Option<Commands>,
+// }
 
-#[derive(Subcommand)]
-enum ValidateSubcommand {
-    /// Display validation to the terminal.
-    Display,
-    /// Write a validation report to a file.
-    Write {
-        #[arg(short, long, value_name = "FILE")]
-        output: PathBuf,
-    },
-}
+// #[derive(Subcommand)]
+// enum Commands {
+//     /// Validate packages in an environment
+//     Validate {
+//         /// File path from which to read bound requirements.
+//         #[arg(short, long, value_name = "FILE")]
+//         bound: Option<PathBuf>,
 
-#[derive(Subcommand)]
-enum ScanSubcommand {
-    /// Display scan to the terminal.
-    Display,
-    /// Write a scan report to a file.
-    Write {
-        #[arg(short, long, value_name = "FILE")]
-        output: PathBuf,
-    },
-}
+//         #[command(subcommand)]
+//         subcommands: ValidateSubcommand,
+//     },
+//     /// Scan environment to report on installed packages.
+//     Scan {
+//         #[command(subcommand)]
+//         subcommands: ScanSubcommand,
+//     },
+//     /// Scan environment to report on installed packages.
+//     Derive {
+//         // Select the nature of the bound in the derived requirements.
+//         #[arg(short, long, value_enum)]
+//         anchor: CliAnchor,
 
-// might support output to requirements or toml?
-#[derive(Subcommand)]
-enum DeriveSubcommand {
-    /// Display derive to the terminal.
-    Display,
-    /// Write a derive report to a file.
-    Write {
-        #[arg(short, long, value_name = "FILE")]
-        output: PathBuf,
-    },
-}
+//         #[command(subcommand)]
+//         subcommands: DeriveSubcommand,
+//     },
+//     /// Purge packages that fail validation
+//     Purge {
+//         /// File path from which to read bound requirements.
+//         #[arg(short, long, value_name = "FILE")]
+//         bound: Option<PathBuf>,
+//     },
+// }
 
-//------------------------------------------------------------------------------
+// #[derive(Subcommand)]
+// enum ValidateSubcommand {
+//     /// Display validation to the terminal.
+//     Display,
+//     /// Write a validation report to a file.
+//     Write {
+//         #[arg(short, long, value_name = "FILE")]
+//         output: PathBuf,
+//     },
+// }
 
-// Get a ScanFS, optionally using exe_paths if provided
-fn get_scan(exe_paths: Option<Vec<PathBuf>>) -> Result<ScanFS, String> {
-    if let Some(exe_paths) = exe_paths {
-        ScanFS::from_exes(exe_paths)
-    } else {
-        ScanFS::from_exe_scan()
-    }
-}
+// #[derive(Subcommand)]
+// enum ScanSubcommand {
+//     /// Display scan to the terminal.
+//     Display,
+//     /// Write a scan report to a file.
+//     Write {
+//         #[arg(short, long, value_name = "FILE")]
+//         output: PathBuf,
+//     },
+// }
 
-fn get_dep_manifest(bound: &Option<PathBuf>) -> Result<DepManifest, String> {
-    if let Some(bound) = bound {
-        DepManifest::from_requirements(bound)
-    } else {
-        Err("Invalid bound path".to_string())
-    }
-}
+// // might support output to requirements or toml?
+// #[derive(Subcommand)]
+// enum DeriveSubcommand {
+//     /// Display derive to the terminal.
+//     Display,
+//     /// Write a derive report to a file.
+//     Write {
+//         #[arg(short, long, value_name = "FILE")]
+//         output: PathBuf,
+//     },
+// }
 
-fn main() {
-    let cli = Cli::parse();
-    // we always do a scan; we might cache this
-    let sfs = get_scan(cli.exe).unwrap(); // handle error
+// //------------------------------------------------------------------------------
 
-    match &cli.command {
-        Some(Commands::Validate { bound, subcommands }) => {
-            let dm = get_dep_manifest(bound).unwrap();
-            let include_sites = false;
-            let v = sfs.validate(dm, include_sites);
-            match subcommands {
-                ValidateSubcommand::Display => {
-                    v.display(include_sites);
-                }
-                ValidateSubcommand::Write { output } => {
-                    println!("{:?}", v);
-                }
-            }
-        }
-        Some(Commands::Scan { subcommands }) => match subcommands {
-            ScanSubcommand::Display => {
-                sfs.display();
-            }
-            ScanSubcommand::Write { output } => {
-                sfs.display();
-            }
-        },
-        Some(Commands::Derive {
-            subcommands,
-            anchor,
-        }) => {
-            match subcommands {
-                DeriveSubcommand::Display => {
-                    let dm = sfs.to_dep_manifest((*anchor).into()).unwrap();
-                    dm.display();
-                }
-                DeriveSubcommand::Write { output } => {
-                    let dm = sfs.to_dep_manifest((*anchor).into()).unwrap();
-                    // TODO: might have a higher-order func that branches based on extension between txt and json
-                    let _ = dm.to_requirements(output);
-                }
-            }
-        }
-        Some(Commands::Purge { bound }) => {
-            let dm = get_dep_manifest(bound);
-            println!("purge");
-        }
+// // Get a ScanFS, optionally using exe_paths if provided
+// fn get_scan(exe_paths: Option<Vec<PathBuf>>) -> Result<ScanFS, String> {
+//     if let Some(exe_paths) = exe_paths {
+//         ScanFS::from_exes(exe_paths)
+//     } else {
+//         ScanFS::from_exe_scan()
+//     }
+// }
 
-        None => {}
-    }
-}
+// fn get_dep_manifest(bound: &Option<PathBuf>) -> Result<DepManifest, String> {
+//     if let Some(bound) = bound {
+//         DepManifest::from_requirements(bound)
+//     } else {
+//         Err("Invalid bound path".to_string())
+//     }
+// }
+
+// fn main() {
+//     let cli = Cli::parse();
+//     // we always do a scan; we might cache this
+//     let sfs = get_scan(cli.exe).unwrap(); // handle error
+
+//     match &cli.command {
+//         Some(Commands::Validate { bound, subcommands }) => {
+//             let dm = get_dep_manifest(bound).unwrap();
+//             let include_sites = false;
+//             let v = sfs.validate(dm, include_sites);
+//             match subcommands {
+//                 ValidateSubcommand::Display => {
+//                     v.display(include_sites);
+//                 }
+//                 ValidateSubcommand::Write { output } => {
+//                     println!("{:?}", v);
+//                 }
+//             }
+//         }
+//         Some(Commands::Scan { subcommands }) => match subcommands {
+//             ScanSubcommand::Display => {
+//                 sfs.display();
+//             }
+//             ScanSubcommand::Write { output } => {
+//                 sfs.display();
+//             }
+//         },
+//         Some(Commands::Derive {
+//             subcommands,
+//             anchor,
+//         }) => {
+//             match subcommands {
+//                 DeriveSubcommand::Display => {
+//                     let dm = sfs.to_dep_manifest((*anchor).into()).unwrap();
+//                     dm.display();
+//                 }
+//                 DeriveSubcommand::Write { output } => {
+//                     let dm = sfs.to_dep_manifest((*anchor).into()).unwrap();
+//                     // TODO: might have a higher-order func that branches based on extension between txt and json
+//                     let _ = dm.to_requirements(output);
+//                 }
+//             }
+//         }
+//         Some(Commands::Purge { bound }) => {
+//             let dm = get_dep_manifest(bound);
+//             println!("purge");
+//         }
+
+//         None => {}
+//     }
+// }
