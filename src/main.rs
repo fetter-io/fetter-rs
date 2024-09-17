bac9ad8363c87b4f09e929760f348989600d950b
Christopher Ariza <ariza@flexatone.com>
2024-09-17 09:28:40 -0700

    added notes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 use std;
 
-// // NEXT:
-// // Packages collect url for @ validations
-// // Implement a colorful display
-// // Implement a monitoring mode
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     fetter::run_cli(std::env::args_os());
     Ok(())
