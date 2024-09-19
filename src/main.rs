c13180715de800e39e39e71db9fb7818b9921934
Christopher Ariza <ariza@flexatone.com>
2024-09-18 18:16:48 -0700

    code cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,3 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     fetter::run_cli(std::env::args_os());
     Ok(())
 }
-
-// TODO:
-// to_validation_report takes a struct of flags
-// test perimit_unspecified
-// expose cli args
