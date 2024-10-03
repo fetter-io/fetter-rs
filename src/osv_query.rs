28f17876bdaf1a88a25e617600724ed0e21aeb56
Christopher Ariza <ariza@flexatone.com>
2024-10-02 17:01:32 -0700

    query_osv now takes Package

--- src/osv_query.rs
+++ src/osv_query.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 use ureq;
 
 // use crate::package::Package;
-use crate::ureq_client::UreqClient;
+use crate::{package::Package, ureq_client::UreqClient};
 
 //------------------------------------------------------------------------------
 // see https://google.github.io/osv.dev/post-v1-querybatch/
@@ -21,6 +21,18 @@ struct OSVPackageQuery {
     package: OSVPackage,
     version: String,
     // note: commit can go here
+}
+
+impl OSVPackageQuery {
+    fn from_package(package: &Package) -> Self {
+        OSVPackageQuery {
+            package: OSVPackage {
+                name: package.name.clone(),
+                ecosystem: "PyPI".to_string(),
+            },
+            version: package.version.to_string(),
+        }
+    }
 }
 
 /// OSV request component
@@ -91,10 +103,15 @@ fn query_osv_batch<U: UreqClient + std::marker::Sync>(
 
 fn query_osv<U: UreqClient + std::marker::Sync>(
     client: &U,
-    packages: Vec<OSVPackageQuery>,
+    packages: &Vec<Package>,
 ) -> Vec<Option<Vec<String>>> {
+    let packages_osv: Vec<OSVPackageQuery> = packages
+        .iter()
+        .map(|p| OSVPackageQuery::from_package(p))
+        .collect();
+
     // par_chunks sends groups of 4 to batch query
-    let results: Vec<Option<Vec<String>>> = packages
+    let results: Vec<Option<Vec<String>>> = packages_osv
         .par_chunks(4)
         .flat_map(|chunk| query_osv_batch(client, chunk))
         .collect();
@@ -116,25 +133,12 @@ mod tests {
             mock_response : "{\"results\":[{\"vulns\":[{\"id\":\"GHSA-34rf-p3r3-58x2\",\"modified\":\"2024-05-06T14:46:47.572046Z\"},{\"id\":\"GHSA-3f95-mxq2-2f63\",\"modified\":\"2024-04-10T22:19:39.095481Z\"},{\"id\":\"GHSA-48cq-79qq-6f7x\",\"modified\":\"2024-05-21T14:58:25.710902Z\"}]},{\"vulns\":[{\"id\":\"GHSA-pmv9-3xqp-8w42\",\"modified\":\"2024-09-18T19:36:03.377591Z\"}]}]}".to_string(),
         };
         // let client = UreqClientLive;
-
         let packages = vec![
-            OSVPackageQuery {
-                package: OSVPackage {
-                    name: "gradio".to_string(),
-                    ecosystem: "PyPI".to_string(),
-                },
-                version: "4.0.0".to_string(),
-            },
-            OSVPackageQuery {
-                package: OSVPackage {
-                    name: "mesop".to_string(),
-                    ecosystem: "PyPI".to_string(),
-                },
-                version: "0.11.1".to_string(),
-            },
+            Package::from_name_version_durl("gradio", "4.0.0", None).unwrap(),
+            Package::from_name_version_durl("mesop", "0.11.1", None).unwrap(),
         ];
 
-        let results = query_osv(&client, packages.clone());
+        let results = query_osv(&client, &packages);
 
         assert_eq!(results.len(), 2);
         assert_eq!(
