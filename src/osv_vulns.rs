54f05a2bbcbb61b239ab2e7743fa254c75108a03
Christopher Ariza <ariza@flexatone.com>
2024-10-03 17:06:15 -0700

    progress on vuln loading

--- src/osv_vulns.rs
+++ src/osv_vulns.rs
@@ -1,138 +1,172 @@
 use rayon::prelude::*;
 use serde::Deserialize;
 use std::collections::HashMap;
-use std::sync::Mutex;
-use ureq::Agent;
-
-
+// use std::sync::Mutex;
+use std::fmt;
+use ureq;
 
 #[derive(Debug, Deserialize)]
 struct OSVVulnReference {
     url: String,
-    // type
+    r#type: String,
+}
+
+impl fmt::Display for OSVVulnReference {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}: {}", self.r#type, self.url)
+    }
 }
 
 #[derive(Debug, Deserialize)]
-struct OSVEcoSpecific {
-    severity: String,
+struct OSVVulnReferences(Vec<OSVVulnReference>);
+
+impl fmt::Display for OSVVulnReferences {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // NOTE: might only show ADVISORY if defined
+        write!(
+            f,
+            "{}",
+            self.0
+                .iter()
+                .map(|c| c.to_string())
+                .collect::<Vec<_>>()
+                .join(", ")
+        )
+    }
 }
 
+// #[derive(Debug, Deserialize)]
+// struct OSVEcoSpecific {
+//     severity: String,
+// }
+
+// #[derive(Debug, Deserialize)]
+// struct OSVAffected {
+//     ecosystem_specific: Option<OSVEcoSpecific>,
+//     // package
+//     // ranges
+//     // database_specific
+// }
 
 #[derive(Debug, Deserialize)]
-struct OSVAffected {
-    ecosystem_specific: OSVEcoSpecific,
-    // package
-    // ranges
-    // database_specific
+struct OSVSeverity {
+    r#type: String,
+    score: String,
+}
+
+impl fmt::Display for OSVSeverity {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}: {}", self.r#type, self.score)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct OSVSeverities(Vec<OSVSeverity>);
+
+impl fmt::Display for OSVSeverities {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // NOTE: might only show the highest CVSS version (CVSS_V3, CVSS_V4)
+        write!(
+            f,
+            "{}",
+            self.0
+                .iter()
+                .map(|c| c.to_string())
+                .collect::<Vec<_>>()
+                .join(", ")
+        )
+    }
 }
 
 #[derive(Debug, Deserialize)]
 struct OSVVulnInfo {
     summary: String,
-    details: String,
-    references: Vec<OSVVulnReference>,
-    affected: Vec<OSVAffected>, // surprised this is an array of affected
+    // details: String,
+    references: OSVVulnReferences,
+    // affected: Vec<OSVAffected>, // surprised this is an array of affected
+    severity: OSVSeverities,
 }
 
+fn query_osv_vuln(vuln_id: &str) -> Option<OSVVulnInfo> {
+    let url = format!("https://api.osv.dev/v1/vulns/{}", vuln_id);
 
-// fn query_osv_vuln(agent: &Agent, vuln_id: &str) -> Option<OSVVulnInfo> {
-//     let url = "https://api.osv.dev/v1/vulns";
-
-//     let response = agent.post(url).send_json(ureq::json!({
-//         "vuln_ids": [vuln_id]
-//     }));
-
-//     match response {
-//         Ok(res) => {
-//             if res.ok() {
-//                 let response_data: HashMap<String, Vec<OSVVulnInfo>> =
-//                     res.into_json().ok()?;
-//                 // OSV returns a map with "vulns" key
-//                 response_data
-//                     .get("vulns")
-//                     .and_then(|vulns| vulns.get(0).cloned())
-//             } else {
-//                 None
-//             }
-//         }
-//         Err(_) => None,
-//     }
-// }
-
-fn query_osv_vuln<U: UreqClient + std::marker::Sync>(
-    client: &U,
-    vuln_id: &str,
-) -> Vec<Option<Vec<String>>> {
-    let url = "https://api.osv.dev/v1/vulns";
-
-    let vuln_query = OSVQueryBatch {
-        queries: packages.to_vec(),
-    };
-    let body = serde_json::to_string(&vuln_query).unwrap();
-    // println!("{:?}", body);
-
-    let response: Result<String, ureq::Error> = client.post(url, &body);
-    match response {
-        Ok(body_str) => {
-            // let body_str = body.into_string().unwrap_or_default();
-            // println!("{:?}", body_str);
-            let osv_res: OSVResponse = serde_json::from_str(&body_str).unwrap();
-
-            osv_res
-                .results
-                .iter()
-                .map(|result| {
-                    result.vulns.as_ref().map(|vuln_list| {
-                        vuln_list
-                            .iter()
-                            .map(|v| v.id.clone())
-                            .collect::<Vec<String>>()
-                    })
-                })
-                .collect()
+    match ureq::get(&url).call() {
+        Ok(response) => {
+            if let Ok(body_str) = response.into_string() {
+                // println!("body_str: {:?}", body_str);
+                let osv_vuln: OSVVulnInfo = serde_json::from_str(&body_str).unwrap();
+                Some(osv_vuln)
+            } else {
+                None
+            }
         }
-        Err(_) => {
-            vec![None; packages.len()]
-        }
+        Err(_) => None,
     }
 }
 
+// fn query_osv_vulns(vuln_ids: Vec<String>) -> HashMap<String, Option<OSVVulnInfo>> {
+//     // let results = Mutex::new(HashMap::new());
 
-fn query_osv_vulns(vuln_ids: Vec<String>) -> HashMap<String, Option<OSVVulnInfo>> {
-    let agent = ureq::agent();
-    let results = Mutex::new(HashMap::new());
+//     // vuln_ids.par_iter().for_each(|vuln_id| {
+//     //     let info = query_osv_vuln(vuln_id);
+//     //     let mut results = results.lock().unwrap();
+//     //     results.insert(vuln_id.clone(), info);
+//     // });
 
-    vuln_ids.par_iter().for_each(|vuln_id| {
-        let info = query_osv_vuln(&agent, vuln_id);
-        let mut results = results.lock().unwrap();
-        results.insert(vuln_id.clone(), info);
-    });
+//     // results.into_inner().unwrap()
+// }
 
-    results.into_inner().unwrap()
+fn query_osv_vulns(vuln_ids: Vec<String>) -> HashMap<String, OSVVulnInfo> {
+    let results: Vec<(String, OSVVulnInfo)> = vuln_ids
+        .par_iter()
+        .filter_map(|vuln_id| query_osv_vuln(vuln_id).map(|info| (vuln_id.clone(), info)))
+        .collect();
+
+    // Convert the Vec to a HashMap after multi-threading
+    results.into_iter().collect()
 }
 
-fn main() {
-    let vuln_ids = vec![
-        "CVE-2021-44228".to_string(),
-        "CVE-2022-22965".to_string(),
-        // add more ids here
-    ];
+//--------------------------------------------------------------------------
 
-    let result_map = query_osv_vulns(vuln_ids);
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    for (vuln_id, info) in result_map {
-        println!("Vuln ID: {}", vuln_id);
-        match info {
-            Some(vuln) => {
-                println!("Summary: {:?}", vuln.summary);
-                println!("Details: {:?}", vuln.details);
-                println!("References: {:?}", vuln.references);
-                println!("Severity: {:?}", vuln.severity);
-            }
-            None => {
-                println!("No data found for this vulnerability.");
-            }
+    use crate::ureq_client::UreqClientMock;
+    // use crate::ureq_client::UreqClientLive;
+
+    #[test]
+    fn test_vuln_a() {
+        let vuln_ids = vec![
+            "GHSA-48cq-79qq-6f7x".to_string(),
+            "GHSA-pmv9-3xqp-8w42".to_string(),
+            // add more ids here
+        ];
+
+        let result_map = query_osv_vulns(vuln_ids);
+
+        for (vuln_id, vuln) in result_map {
+            println!("Vuln ID: {}", vuln_id);
+
+            println!("Summary: {:?}", vuln.summary);
+            // println!("Details: {:?}", vuln.details);
+            println!("References: {}", vuln.references);
+            // println!("Affected: {:?}", vuln.affected);
+            println!("Severity: {}", vuln.severity);
+
+            // match info {
+            //     Some(vuln) => {
+            //         println!("Summary: {:?}", vuln.summary);
+            //         // println!("Details: {:?}", vuln.details);
+            //         println!("References: {:?}", vuln.references);
+            //         // println!("Affected: {:?}", vuln.affected);
+            //         println!("Severity: {:?}", vuln.severity);
+            //     }
+            //     None => {
+            //         println!("No data found for this vulnerability.");
+            //     }
+            // }
+            println!();
         }
-        println!();
     }
 }
