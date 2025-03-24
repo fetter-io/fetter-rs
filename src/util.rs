92ef6545a2d8a773285b2b51608ceb352ccbfd2a
afares97 <andrew.fares97@yahoo.com>
2025-03-24 13:42:15 -0700

    applied formatting

--- src/util.rs
+++ src/util.rs
@@ -528,7 +528,7 @@ mod tests {
     fn test_parse_conda_filename() {
         let filename = "_libgcc_mutex-0.1-conda_forge.tar.bz2";
         let parsed_filename = parse_conda_filename(filename);
-        
+
         assert_eq!(
             parsed_filename,
             Some(("_libgcc_mutex".to_string(), "0.1".to_string()))
