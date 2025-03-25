c31236eb54d06ad49c99fc12e4919e8413e5ca55
Christopher Ariza <ariza@flexatone.com>
2025-03-24 21:14:13 -0700

    adding failed test

--- src/audit_report.rs
+++ src/audit_report.rs
@@ -177,4 +177,9 @@ mod tests {
         assert_eq!(lines.next().unwrap().unwrap(), "gradio-4.0.0,GHSA-48cq-79qq-6f7x,Reference,https://nvd.nist.gov/vuln/detail/CVE-2024-1727");
         assert_eq!(lines.next().unwrap().unwrap(), "gradio-4.0.0,GHSA-48cq-79qq-6f7x,Severity,CVSS:3.1/AV:N/AC:L/PR:N/UI:R/S:U/C:N/I:N/A:L");
     }
+
+    #[test]
+    fn test_failure() {
+        assert_eq!(1, 0);
+    }
 }
