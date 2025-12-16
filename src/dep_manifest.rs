236cc253714f5d0bd184f34933ab70318066a4b4
Christopher Ariza <ariza@flexatone.com>
2025-12-15 20:18:40 -0800

    sketch

--- src/dep_manifest.rs
+++ src/dep_manifest.rs
@@ -95,7 +95,7 @@ impl DepSpecOOM {
 // A DepManifest is a requirements listing, implemented as HashMap for quick lookup by package name.
 #[derive(Debug, Clone)]
 pub struct DepManifest {
-    dep_specs: HashMap<String, DepSpecOOM>,
+    pub(crate) dep_specs: HashMap<String, DepSpecOOM>,
     pub(crate) env_marker_active: bool,
 }
 
--- src/lookup_report.rs
+++ src/lookup_report.rs
@@ -12,17 +12,14 @@ use crate::CvssFilter;
 use serde::Serialize;
 use std::ops::Deref;
 use std::sync::Arc;
+use crate::util::ResultDynError;
 
-// given a set of packages (with defined specific versions), check if those versions have vulnerabilities; if so provide vulnerability details for each. Vuln details can reuse AuditRecord, AuditReport
-
-// so what we need are two specialized constructors:
-// from name_or_dep_spec(Option<limit>); if a name, get all, if a dep-spec, apply filtering... or just any string can be made into a dep spec
-// from_dep_manifest
 
 #[derive(Debug, Serialize)]
 pub struct LookupReport(pub AuditReport);
 
 impl LookupReport {
+    /// Get a LookupReport from a single `DepSpec`.
     pub fn from_dep_spec(
         client: Arc<dyn UreqClient>,
         ds: &DepSpec,
@@ -75,6 +72,56 @@ impl LookupReport {
             true,
         );
         LookupReport(audit_report)
+    }
+
+    pub fn from_dep_manifest(
+        client: Arc<dyn UreqClient>,
+        file_path: &Path,
+        bound_options: Option<&Vec<String>>,
+        env_marker_state: Option<&EnvMarkerState>,
+        cache_config: &CacheConfig,
+        cache_refresh: FlagCacheRefresh,
+        log: FlagLog,
+    ) -> ResultDynError<Self> {
+        let dm = DepManifest::from_path_or_url(file_path, bound_options.as_ref())?;
+        for key, dsoom in dm.dep_specs {
+            let ds_iter = match dsoom {
+                DepSpecOOM::One(ds) => std::slice::from_ref(ds).iter(),
+                DepSpecOOM::Many(ds_vec) => ds_vec.iter(),
+            };
+            dss: Vec<DepSpec> = Vec::new();
+            for ds in ds_iter {
+                if ds.env_marker.is_empty() {
+                    dss.push(ds);
+                }
+                else {
+                    let ems = env_marker_state.expect("EMS should be loaded");
+                    if ds.validate_env_marker(ems) {
+                        dss.push(ds);
+                    }
+
+                }
+            }
+            for ds in dss {
+                let pypi_project = query_pypi_project(client.clone(), &ds.key, cache_config, log);
+
+                let packages: Vec<Package> = match pypi_project {
+                    Some(project) => project
+                        .get_version_specs(Some(ds), limit)
+                        .into_iter()
+                        .map(|version| Package {
+                            name: ds.name.clone(),
+                            key: name_to_key(&ds.name),
+                            version,
+                            direct_url: None,
+                        })
+                        .collect(),
+                    None => Vec::new(),
+                };
+
+            }
+        }
+
     }
 }
 
